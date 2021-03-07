fb5c076ec76150f24133827eb3caab0b1a5c584b
andows159 <canalandows@gmail.com>
2021-03-06 21:53:48 -0300

    Dart exercises

---
+++ dart_exercises/ex001.dart
@@ -0,0 +1,13 @@
+//dart variables and initial concepts anotation
+
+main() {
+  //dart uses the mathematical order precedence to calc
+  var calc = 23 / 2 + 3;
+  //inferring type automatically
+  var a = 2;
+  //interger type
+  int b = 1;
+  print(a);
+  print(b);
+  print(calc);
+}
---
+++ dart_exercises/ex002.dart
@@ -0,0 +1,7 @@
+//anotation for string and types on dart
+main() {
+  //after infered the type automatically by compiler, will impossible change it
+  var number1 = 2;
+  print(number1.runtimeType);
+  //the string system runs like in python, or at least similar
+}
