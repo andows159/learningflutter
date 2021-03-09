6e03b46840056a3ff056effece37b0415ec3b412
andows159 <canalandows@gmail.com>
2021-03-09 10:02:40 -0300

    Aula 36 modulo 02

--- questions_and_answers/lib/main.dart
+++ questions_and_answers/lib/main.dart
@@ -1,66 +1,65 @@
 import 'package:flutter/material.dart';
-import 'questao.dart';
-import 'resposta.dart';
+import './questao.dart';
+import './resposta.dart';
 
 main() => runApp(PerguntaApp());
 
-//esse "_" antes do nome quer dizer que ele é privado para outros arquivos
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
-  var appBar = 'Normal';
-  //esse metodo, toda vez que for mudada, a build
-  //vai refazer toda a interface grafica (na verdade não toda, mas vai buscar em toda, e mudar oque for necessario)
-  //o flutter é cirurgico nessa parte
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    }
+  ];
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada = _perguntaSelecionada + 1;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita ?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
-      },
-      {
-        'texto': 'Qual seu animal favorito ?',
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
-      },
-      {
-        'texto': 'Qual é o seu instruturo favorito ?',
-        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
-      }
-    ];
-    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'];
-    List<Widget> widgets = [];
-    for (var TextoResp in respostas) {
-      widgets.add(Resposta(TextoResp, _responder));
-    }
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada]['respostas']
+        : null;
+
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: Container(
-                width: double.infinity,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: <Widget>[
+                  Questao(_perguntas[_perguntaSelecionada]['texto']),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : Center(
                 child: Text(
-                  'Perguntas',
-                  textAlign: TextAlign.center,
+                  'Parabéns!',
+                  style: TextStyle(fontSize: 28),
                 ),
               ),
-              backgroundColor: Colors.blue,
-            ),
-            body: Column(children: <Widget>[
-              Questao(perguntas[_perguntaSelecionada]['texto']),
-              ...widgets,
-              /*widgets[0],
-              widgets[1],
-              widgets[2],
-              widgets[3] 
-              
-              observe os três pontos ali em cima que substituem esse método*/
-            ])));
+      ),
+    );
   }
 }
 
